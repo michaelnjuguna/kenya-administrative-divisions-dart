ff8faf9cfe77f6c3ca14f32990b8780e5d1ae3f6
Michael Njuguna <michaelnjuguna184+github@gmail.com>
2026-05-21 23:13:00 +0300

    WIP: getCounties

---
+++ lib/src/actions/get_counties.dart
@@ -0,0 +1,20 @@
+import 'package:kenya_administrative_divisions/src/models.dart';
+import 'package:kenya_administrative_divisions/src/params.dart';
+
+class GetCounties {
+  final List<County> data;
+  final GetCountiesParams? params;
+  GetCounties({required this.data, this.params});
+  List<County> call() {
+    try {
+      final args = params;
+      if (args == null) {
+        return data;
+      }
+      if (args.countyCode != null) {}
+    } catch (e) {
+      throw Exception('GetCounties failed: $e');
+    }
+    return [];
+  }
+}
---
+++ lib/src/params.dart
@@ -0,0 +1,11 @@
+typedef GetCountiesParams = ({int? countyCode, String? countyName});
+typedef GetConstituenciesParams = ({
+  int? countyCode,
+  String? countyName,
+  String? constituencyName,
+});
+typedef GetWardsParams = ({
+  int? countyCode,
+  String? countyName,
+  String? constituencyName,
+});
