9b56baa385bb1c5ff82d36473622facbb0ce345d
Michael Njuguna <michaelnjuguna184+github@gmail.com>
2026-06-11 19:56:33 +0300

    tests: get wards

--- test/get_wards_test.dart
+++ test/get_wards_test.dart
@@ -11,6 +11,7 @@ void main() {
     test('No parameters passed', () {
       final List<Ward> result = kad.getWards();
       expect(result, isNotEmpty);
+      expect(result.first, 'Port Reitz');
     });
     test('Invalid param passed as county code', () {
       expect(() => kad.getWards(countyCode: 0), throwsArgumentError);
@@ -19,6 +20,23 @@ void main() {
     test('Valid param passed as county code', () {
       final List<Ward> result = kad.getWards(countyCode: 1);
       expect(result, isNotEmpty);
+      expect(result.first, 'Port Reitz');
+    });
+    test('Invalid string passed as county name', () {
+      expect(
+          () => kad.getWards(countyName: 'Invalid name'), throwsArgumentError);
+    });
+    test('Valid string passed as county name', () {
+      final List<Ward> result = kad.getWards(countyName: 'mombasa');
+      expect(result, isNotEmpty);
+      expect(result.first, 'Port Reitz');
+    });
+    test('Invalid string passed as constituency name', () {
+      expect(() => kad.getWards(constituencyName: 'Invalid name'),
+          throwsArgumentError);
+    });
+    test('Valid string passed as constituency name', () {
+      List<Ward> result = kad.getWards(constituencyName: 'CHANGAMWE');
       expect(result.first, 'Port Reitz');
     });
   });
